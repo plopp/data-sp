66c3da32c9c83c0f2c96bbbd87d8ddee3008f19a
Marcus Kempe <makem86@gmail.com>
2014-03-12 00:23:31 +0100

    Working favourites

--- client/views/projects_list.js
+++ client/views/projects_list.js
@@ -1,5 +1,29 @@
 Template.projectsList.helpers({
+	favorites : function(){
+		return Projects.find({isFav: true},{sort: {title: 1}});
+	},
 	projects : function(){
-		return Projects.find({});
+		return Projects.find({isFav: false},{sort: {title: 1}});
+	},
+	success : function(){
+		return Projects.findOne(this._id).status == 'success';
+	},
+	warning : function(){
+		return Projects.findOne(this._id).status == 'warning';
+	},
+	isFav : function(){
+		return Projects.findOne(this._id).isFav;
 	}
 });
+
+Template.projectsList.events = {
+	'click .glyphicon-star-empty' : function(e){
+		var id = $(e.target).attr("id");
+		Projects.update(id, {$set: {isFav: true}});
+	},
+	'click .glyphicon-star' : function(e){
+		var id = $(e.target).attr("id");
+		Projects.update(id, {$set: {isFav: false}});
+		
+	}
+}
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,25 +1,29 @@
 if(Projects.find().count() == 0){
 	Projects.insert({
-		name: 'Testbädd solel',
+		name: 'A Testbädd solel',
 		number: '3P07449-01',
-		status: 'success'
+		status: 'success',
+		isFav: true
 	});
 
 	Projects.insert({
-		name: 'Need4b',
+		name: 'B Need4b',
 		number: 'ETk6092-06',
-		status: 'success'
+		status: 'success',
+		isFav: false
 	});
 
 	Projects.insert({
-		name: 'Borås Energi & Miljö',
+		name: 'C Borås Energi & Miljö',
 		number: 'ETk7044-01',
-		status: 'warning'
+		status: 'warning',
+		isFav: false
 	});
 
 	Projects.insert({
-		name: 'Energimyndigheten',
+		name: 'D Energimyndigheten',
 		number: '3P4529',
-		status: 'danger'
+		status: 'danger',
+		isFav: false
 	});
 }
