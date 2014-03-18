1d4775c2c026cfc59765fa8aa26fb973b846a045
Marcus Kempe <makem86@gmail.com>
2014-03-18 08:54:02 +0100

    First commit

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,27 +1,27 @@
 if(Projects.find().count() == 0){
 	Projects.insert({
 		name: 'A Testbädd solel',
-		number: '3P07449-01',
+		number: '3P898765556-01',
 		status: 'success',
 		isFav: true
 	});
 
 	Projects.insert({
-		name: 'B Need4b',
-		number: 'ETk6092-06',
+		name: 'B Testprojekt 2',
+		number: 'ETk66666-06',
 		status: 'success',
 		isFav: false
 	});
 
 	Projects.insert({
-		name: 'C Borås Energi & Miljö',
-		number: 'ETk7044-01',
+		name: 'C Energi & Miljöprojekt',
+		number: 'ETk88888-01',
 		status: 'warning',
 		isFav: false
 	});
 
 	Projects.insert({
-		name: 'D Energimyndigheten',
+		name: 'D Myndighet',
 		number: '3P4529',
 		status: 'danger',
 		isFav: false
