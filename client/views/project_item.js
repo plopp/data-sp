6dbfac5da3c61ef76ed8e834f537cbd8dbd765de
Marcus Kempe <makem86@gmail.com>
2014-03-18 23:33:31 +0100

    Partly implemented map modal

--- client/views/project_item.js
+++ client/views/project_item.js
@@ -20,10 +20,34 @@ Template.projectItem.helpers({
 	}
 });
 
-var rendered = false;
+Deps.autorun(function(){
+	
+});
+
 Template.projectItem.rendered = function(){
-	if(!rendered){
-		$("span").not("span:last").append("<hr>");
-		rendered = true;
-	}
+	
 }
+
+Template.projectItem.created = function(){
+
+}
+
+Template.projectItem.events({
+	'click .butt' : function(){
+		$("#myModal").modal('show');
+		  // var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+		  // var mapOptions = {
+		  //   zoom: 4,
+		  //   center: myLatlng,
+		  //   mapTypeId: google.maps.MapTypeId.ROADMAP
+		  // }
+		  // var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+		  // var marker = new google.maps.Marker({
+		  //     position: myLatlng,
+		  //     map: map,
+		  //     title: 'Hello World!'
+		  // });
+		
+	}
+});
